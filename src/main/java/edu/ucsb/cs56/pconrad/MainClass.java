be9b9b3a518dbe0ebf4ef62c65aaf7e05488e217
unknown <nuankw@yahoo.com>
2018-09-14 11:43:48 -0700

    commit before merge

--- src/main/java/edu/ucsb/cs56/pconrad/MainClass.java
+++ src/main/java/edu/ucsb/cs56/pconrad/MainClass.java
@@ -2,30 +2,38 @@ package edu.ucsb.cs56.pconrad;
 
 import static spark.Spark.port;
 
+import java.util.HashMap;
+import java.util.Map;
 
-/**
- * Hello world!
- *
- */
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
 
-public class SparkDemo01 {
+
+public class MainClass {
+
     public static void main(String[] args) {
 
         port(getHerokuAssignedPort());
-		
+
 		System.out.println("");
 		System.out.println("(Don't worry about the warnings below about SLF4J... we'll deal with those later)");
-		System.out.println("");						  
+		System.out.println("");
 		System.out.println("In browser, visit: http://localhost:" + getHerokuAssignedPort() + "/hello");
 		System.out.println("");
-        spark.Spark.get("/", (req, res) -> "Hello World");
-		spark.Spark.get("/hello", (req, res) -> "Hello World");
-		spark.Spark.get("/bye", (req, res) -> "Goodbye World");
-		spark.Spark.get("/yo", (req, res) -> "S'up World");
-		spark.Spark.get("/tension", (req, res) -> "Midterm next week.  No problem dude");		
+    spark.Spark.get("/", (req, res) -> {res.redirect("/todo");return null;});
+    Map taskList = new HashMap();
+    taskList.put("task", "One");
+    taskList.put("task", "Two");
+    taskList.put("task", "Three");
+
+  //   spark.Spark.get("/todo", (rq, rs) -> {
+  //
+  //     return new ModelAndView(taskList, "todo.mustache");
+  //   },
+  //   new MustacheTemplateEngine() );
 
 	}
-	
+
     static int getHerokuAssignedPort() {
         ProcessBuilder processBuilder = new ProcessBuilder();
         if (processBuilder.environment().get("PORT") != null) {
@@ -34,5 +42,5 @@ public class SparkDemo01 {
         return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 
-	
+
 }
